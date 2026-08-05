1fe3279fec67d10434206dffe8f7717f0bc89adb
Sherif Osama <sherifosama172@gmail.com>
2026-08-05 15:04:18 +0300

    Add role service interface

--- School.BLL/Interfaces/IRoleService.cs
+++ School.BLL/Interfaces/IRoleService.cs
@@ -1,7 +1,14 @@
-﻿namespace School.BLL.Interfaces
+﻿using School.DTO.RoleDTOs;
+
+namespace School.BLL.Interfaces
 {
     public interface IRoleService
     {
-
+        Task<List<RoleDTO>> GetAllRolesAsync();
+        Task<RoleDTO?> GetRoleByIdAsync(int roleId);
+        Task<RoleDTO?> GetRoleByNameAsync(string roleName);
+        Task<int> AddRoleAsync(RoleDTO role);
+        Task<bool> UpdateRoleAsync(RoleDTO role);
+        Task<bool> DeleteRoleAsync(int roleId);
     }
 }
